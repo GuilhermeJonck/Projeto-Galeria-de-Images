b09e7f925738d8debc1b3f4e7e176ba0889e44ee
Guilherme J <guijonckk@gmail.com>
2024-04-02 19:52:02 -0300

    btn / light

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+    //substituir a image
+}
+  
 lightbox.option({
     resizeDuration: 200,
     wrapAround: true,
     disableScrolling: true,
 })
+
